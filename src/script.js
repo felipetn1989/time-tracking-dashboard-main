608ccfbdd452c892b2d1b75f3a3f7f3ebb23c447
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-03-10 16:39:24 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,25 +1,12 @@
+const selectTime = document.querySelectorAll(".select_timeframe");
+let selectedIndex = 0;
+
 async function data() {
   const response = await fetch("https://api.npoint.io/7c3c76337e088202d05b");
   const json = await response.json();
 
-  const selectTime = document.querySelectorAll(".select_timeframe");
-  let selectedIndex = 1
-
-  selectTime.forEach((period,index) => {
-    period.addEventListener("click", (event) => {;
-      period.classList.toggle("selected");
-      selectedIndex = index
-      console.log(selectedIndex)
-      for (let j = 0; j < selectTime.length; j++) {
-        if (selectTime[j] !== event.target) {
-          selectTime[j].classList.remove("selected")
-        }
-      }
-    });
-  });
-
-  let frames = ["daily","weekly","monthly"]
-
+  const period = ["daily","weekly","monthly"]
+  const time = ["Day","Week","Month"]
 
   for (let i = 0; i < json.length; i++) {
     let colors = [
@@ -34,13 +21,21 @@ async function data() {
     trackingBlocks.innerHTML += `
         <div class="grid text-[#bdc1ff]">
           <div class="block_header grid translate-y-2 h-20 w-full rounded-2xl overflow-hidden z-0">
-            <img class="place-self-end pr-4 translate-y-[-0.6875rem]" src="${json[i].image}" alt="">
+            <img class="place-self-end pr-4 translate-y-[-0.6875rem]" src="${
+              json[i].image
+            }" alt="">
           </div>
           <div class="block_main grid gap-y-3 grid-cols-2 bg-[#1c1f4a] px-6 pt-[1.625rem] pb-9 z-10 rounded-2xl translate-y-[-2rem]">
-            <span class="font-bold text-white tracking-tight">${json[i].title}</span>
+            <span class="font-bold text-white tracking-tight">${
+              json[i].title
+            }</span>
             <img class="ellipsis" src="../images/icon-ellipsis.svg" alt="" aria-hidden="true">
-            <span class="hours leading-6 text-white text-3xl font-light tracking-tight">${json[i].timeframes.weekly.current}hrs</span>
-            <span class="place-self-end text-sm">Last Week - ${json[i].timeframes.weekly.previous}hrs </span>
+            <span class="hours leading-6 text-white text-3xl font-light tracking-tight">${
+              json[i].timeframes[period[selectedIndex]]["current"]
+            }hrs</span>
+            <span class="place-self-end text-sm">Last ${time[selectedIndex]} - ${
+              json[i].timeframes[period[selectedIndex]]["previous"]
+            }hrs </span>
           </div>
         </div>
       `;
@@ -50,12 +45,26 @@ async function data() {
       header.style.backgroundColor = colors[index];
     });
 
-    const blockMains = document.querySelectorAll(".block_main") 
-    blockMains.forEach (main => {
-      main.classList.add("hover:cursor-pointer")
+    const blockMains = document.querySelectorAll(".block_main");
+    blockMains.forEach((main) => {
+      main.classList.add("hover:cursor-pointer");
       main.classList.add("hover:bg-[#6f76c8]");
-    })
+    });
   }
 }
 
 data();
+
+selectTime.forEach((period, index) => {
+  period.addEventListener("click", (event) => {
+    period.classList.toggle("selected");
+    selectedIndex = index;
+    console.log(selectedIndex);
+    for (let j = 0; j < selectTime.length; j++) {
+      if (selectTime[j] !== event.target) {
+        selectTime[j].classList.remove("selected");
+      }
+    }
+    data()
+  });
+});
